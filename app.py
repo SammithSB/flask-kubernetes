58919c6ebaba4afe71fd8a1d7891c8c58c9ac8cf
SammithSB <sammithsb@gmail.com>
2023-01-03 18:09:01 +0530

    init commit with view items done

---
+++ app.py
@@ -0,0 +1,51 @@
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    description = db.Column(db.Text)
+    price = db.Column(db.Float)
+
+    def __init__(self, title, description, price):
+        self.title = title
+        self.description = description
+        self.price = price
+
+@app.route('/')
+def index():
+    items = Item.query.all()
+    return render_template('home.html', items=items)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_item():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        price = request.form['price']
+        new_item = Item(title, description, price)
+        db.session.add(new_item)
+        db.session.commit()
+        return redirect(url_for('home'))
+    else:
+        return render_template('add_item.html')
+
+@app.route('/delete/<int:id>')
+def delete_item(id):
+    item = Item.query.get(id)
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
